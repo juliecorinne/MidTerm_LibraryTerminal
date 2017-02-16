5d7606aa8c1dcbd733a4d30d3d44a00f49c796dc
rteach <ross.c.teach@gmail.com>
2017-02-16 09:06:51 -0500

    update method for reading .txt file

--- src/BookDB.java
+++ src/BookDB.java
@@ -1,7 +1,26 @@
-/**
- * Created by julieschneider on 2/15/17.
- */
+import java.util.ArrayList;
+
 public class BookDB {
+    private ArrayList<Book> collection;
+
+    //constructor
+    public BookDB(){
+        this.collection = new ArrayList<Book>();
+    }
+
+    public ArrayList<Book> getCollection() {
+        return collection;
+    }
+
+    public void setCollection(ArrayList<Book> collection) {
+        this.collection = collection;
+    }
+
+    public void addBook(Book book) {
+        this.collection.add(book);
+    }
+
+
 
 
 
